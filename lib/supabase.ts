9f704f39c08c3eb2ce5a9e18c24548a37f93ba54
Agustina Clarens <agustinaclarens@Mac.fibertel.com.ar>
2026-06-29 20:25:52 -0300

    fix supabase url

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = "https://ddkccvqthljgvrvcjomi.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRka2NjdnF0aGxqZ3ZydmNqb21pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MzIzNjYsImV4cCI6MjA5NDIwODM2Nn0.hlLdptPk3C3PmmdS0Of_qcOjYRStQyW7Ch37PrfpzxI"
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  "https://ultuedaypliljfnspthu.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsdHVlZGF5cGxpbGpmbnNwdGh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0MTYxMjQsImV4cCI6MjA5Nzk5MjEyNH0.Hh-mlnQRI9SegfvFpmH-QtGKSOoXRThTmqLN0Bd6cBA"
+)
